0b6511a82e92016ca24dfbb9c53b116a197a015b
KEITH WINFIELD <30985734+kwinfield@users.noreply.github.com>
2022-05-02 13:43:32 -0400

    updating about and portfolio section

--- src/components/middle/about/About.jsx
+++ src/components/middle/about/About.jsx
@@ -7,10 +7,10 @@ const About = () => (
         <h5>My Story</h5>
         <div className="row pt-5">
             <div className="col-md-6">
-                <p>I am a Web Developer out of Baltimore, Maryland. I deliver both strategic insight, hands-on design and frontend development using modern frontend web technologies. Working as Web Manager for a nationwide wholesale supplier has also provided me with extensive experience in running digital marketing campains using technologies like Google Analytics, Google Adsense, and Mailchimp.</p>
+                <p>I am a Web Developer out of Baltimore, Maryland. I deliver strategic insight, hands-on design, and frontend development using modern frontend web technologies. Working as a Web Manager for a nationwide wholesale supplier has also provided me with extensive experience running digital marketing campaigns using technologies like Google Analytics, Google Adsense, and Mailchimp.</p>
             </div>
             <div className="col-md-6">
-                <p>My background in digital, direct and customer relationship marketing, goes back as far as the early 2000's when I was a young creator for small start ups interested in creating a web presence. It has always been a hobbie of mind to take things apart just to build them again. That inquisitiveness has helped me learn and continue to learn technologies that interest me. ​</p>
+                <p>My background in digital, direct, and customer relationship marketing goes back as far as the early 2000 when I was a young creator for small start-ups interested in creating a web presence. It has always been a hobby of mind to take things apart and build them again. That inquisitiveness has helped me learn and continue to learn technologies that interest me.  ​</p>
             </div>
         </div>
     </div>
--- src/components/middle/portfolio/Portfolio.jsx
+++ src/components/middle/portfolio/Portfolio.jsx
@@ -12,7 +12,13 @@ const Portfolio = () => (
                     <div className="card-body">
                         <h5 className="card-title">R.E. Michel</h5>
                         <p className="card-text">Founded in 1935, R.E. Michel is a family business that has more than 290 sales locations coast to coast.</p>
-                        <a href="#" className="btn btn-primary" data-toggle="modal" data-target="#remodal">Project Info</a>
+                        <h5>Highlights</h5>
+                        <ul className="text-light">
+                            <li>Upgraded to fully responsive</li>
+                            <li>SEO Improvements</li>
+                            <li>Ongoing Theme Maintenance & Support</li>
+                        </ul>
+                        <a href="https://www.remichel.com" className="btn btn-primary" target="_blank">Live Project</a>
                     </div>
                 </div>
             </div>
@@ -22,47 +28,13 @@ const Portfolio = () => (
                     <div className="card-body">
                         <h5 className="card-title">Airtemp</h5>
                         <p className="card-text">Airtemp indoor comfort systems are designed, assembled and repeatedly and thoroughly tested in North America. </p>
-                        <a href="#" className="btn btn-primary" data-toggle="modal" data-target="#atmodal">Project Info</a>
-                    </div>
-                    {/* modal 1 */}
-                    <div className="modal fade" id="remodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div className="modal-dialog" role="document">
-                            <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title" id="exampleModalLabel">R.E. Michel</h5>
-                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                                ...
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" className="btn btn-primary">Save changes</button>
-                            </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* modal 2 */}
-                    <div className="modal fade" id="atmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div className="modal-dialog" role="document">
-                            <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                                ...
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" className="btn btn-primary">Save changes</button>
-                            </div>
-                            </div>
-                        </div>
+                        <h5>Highlights</h5>
+                        <ul className="text-light">
+                            <li>Upgraded to fully responsive</li>
+                            <li>SEO Improvements</li>
+                            <li>Ongoing Theme Maintenance & Support</li>
+                        </ul>
+                        <a href="https://airtemp.com/" className="btn btn-primary" target='_blank'>Live Project</a>
                     </div>
                 </div>
             </div>
