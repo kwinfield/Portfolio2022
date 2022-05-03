825842408c5aba6ea7aa7080cc372c77877ceb7f
KEITH WINFIELD <30985734+kwinfield@users.noreply.github.com>
2022-05-03 15:15:36 -0400

    added initial icon/logo

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -10,7 +10,7 @@ function Navbar (){
         <div>
             <div className="nav1">
                 <nav className="navbar navbar-light mt-3">
-                    <a className="navbar-brand text-light">KW</a>
+                    <a className="navbar-brand text-light"><img src="/images/portfolio-logo.jpg" alt="" /></a>
                     <ul className="nav justify-content-end">
                         {menu.map((item)=> (<li className="nav-item"><a className="nav-link" href={`#${item}`}>{item}</a></li>))}
                     </ul>
@@ -18,7 +18,7 @@ function Navbar (){
             </div>
             <div className="nav2">
                 <nav class="navbar">
-                    <a class="navbar-brand" href="#">KW</a>
+                    <a class="navbar-brand" href="#"><img src="/images/portfolio-logo.jpg" alt="" /></a>
                     <button class="navbar-toggler third-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
                     aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation">
                     <div class="animated-icon3"><span></span><span></span><span></span></div>
