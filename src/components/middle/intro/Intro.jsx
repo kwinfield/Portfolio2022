4365133d698da07ceedf590ed9aa33e169409ae7
KEITH WINFIELD <30985734+kwinfield@users.noreply.github.com>
2022-05-09 09:35:58 -0400

    fixed hamburger dropdown and card-navs

--- src/components/middle/intro/Intro.jsx
+++ src/components/middle/intro/Intro.jsx
@@ -4,16 +4,17 @@ import 'animate.css';
 
 const Intro = () => (
     <div className="intro" id="home">
-        
+
         <div className="opening">
         <h1 className="initials animate__animated animate__fadeIn">Keith<br></br> Winfield</h1>
             <div className="message">
                 <h1 className="animate__animated animate__bounceInDown animate__delay-1s">Web Developer</h1>
                 <h4 className="animate__animated animate__bounceInRight animate__delay-2s">Front End Web Developer specializing in Content Management, Website Optimization, Email Marketing and Social Media Marketing.</h4>
             </div>
-            <a href="#contact" className="btn btn-lg btn-outline-light mt-5 animate__animated animate__bounceInUp animate__delay-3s">Contact Me </a>
+            <a href="#contact" className="btn btn-lg btn-outline-light mt-5 animate__animated animate__bounceInUp animate__delay-3s big-btn">Contact Me </a>
+            <a href="#contact" className="btn btn-lg btn-outline-light mt-5 sm-btn">Contact Me </a>
         </div>
-        
+
     </div>
 )
 
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -8,31 +8,31 @@ const menu = ["home", "about", "services", "portfolio", "contact"];
 function Navbar (){
     return (
         <div>
-            <div className="nav1">
-                <nav className="navbar navbar-light mt-3">
-                    <a className="navbar-brand text-light"><img src="/images/portfolio-logo.jpg" alt="" /></a>
-                    <ul className="nav justify-content-end">
-                        {menu.map((item)=> (<li className="nav-item"><a className="nav-link" href={`#${item}`}>{item}</a></li>))}
-                    </ul>
-                </nav>
-            </div>
-            <div className="nav2">
-                <nav class="navbar">
-                    <a class="navbar-brand" href="#"><img src="/images/portfolio-logo.jpg" alt="" /></a>
-                    <button class="navbar-toggler third-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
-                    aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation">
-                    <div class="animated-icon3"><span></span><span></span><span></span></div>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent1">
-                        <ul className="navbar-nav mr-auto">
-                            {menu.map((item)=> (<li className="nav-item"><a className="nav-link" href={`#${item}`}>{item}</a></li>))}
-                        </ul>
-                    </div>
-                </nav>
-            </div>
+          <div className="nav1">
+              <nav className="navbar navbar-light mt-3">
+                  <a className="navbar-brand text-light"><img src="/images/portfolio-logo.jpg" alt="" /></a>
+                  <ul className="nav justify-content-end">
+                      {menu.map((item)=> (<li className="nav-item"><a className="nav-link" href={`#${item}`}>{item}</a></li>))}
+                  </ul>
+              </nav>
+          </div>
+          <div className="nav2">
+              <nav class="navbar">
+                  <a class="navbar-brand" href="#"><img src="/images/portfolio-logo.jpg" alt="" /></a>
+                  <button class="navbar-toggler third-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
+                  aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation">
+                  <div class="animated-icon3"><span></span><span></span><span></span></div>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent1">
+                      <ul className="navbar-nav mr-auto">
+                          {menu.map((item)=> (<li className="nav-item"><a className="nav-link" href={`#${item}`}>{item}</a></li>))}
+                      </ul>
+                  </div>
+              </nav>
+          </div>
         </div>
-        
-            
+
+
         )
 }
 
