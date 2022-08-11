8ff136142e0064cdfa48477f04fe2af08345d61d
KEITH WINFIELD <30985734+kwinfield@users.noreply.github.com>
2022-08-11 13:13:08 -0400

    adjusted portfolio

---
+++ src/components/middle/portfolio/cards/card2.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import projects from '../../../projects.js';
+import './card.scss';
+
+function Card() {
+  return (
+    <div className='row'>
+        {projects.map(project => {
+            return <div className="card col-5 mb-2">
+                        <img className="card-img-top img-fluid pt-2" src={project.imgURL} alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title text-center">{project.name}</h5>
+                            <div className="text-center">
+                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target={project.modalIDbutton}>
+                                Project Info
+                                </button>
+                            </div>
+                        </div>
+                        {/* <!-- Modal --> */}
+                        <div class="modal fade" id={project.modalID} tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                       <div>
+                                            <div className="row">
+                                                <div className='col-10 mx-auto mb-5'>
+                                                    <img className="card-img-top img-fluid pt-2" src={project.beforeAfter} alt="Card image cap" />
+                                                </div>
+                                            </div>
+                                            <h5>{project.name}:</h5>
+                                            <p className="card-text text-dark">{project.description}</p>
+                                            <h5>Work Done:</h5>
+                                            <ul className="text-dark">
+                                                <li>{project.highlights[0]}</li>
+                                                <li>{project.highlights[1]}</li>
+                                                <li>{project.highlights[2]}</li>
+                                            </ul> 
+                                            <a href={project.link} className="btn btn-light" target="_blank" rel="noreferrer">Live Project</a>
+                                       </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        })}
+    </div>
+  );
+}
+
+export default Card;
--- src/components/projects.js
+++ src/components/projects.js
@@ -4,24 +4,44 @@ const projects = [
     name: "R.E. Michel",
     imgURL: "/images/remlogo.png",
     description: "Founded in 1935, R.E. Michel is a family business that has more than 290 sales locations coast to coast.",
-    highlights: ["Upgraded to fully responsive", "SEO Improvements and Management", "Ongoing Theme Maintenance & Support"],
-    link: "https://www.remichel.com/Webservices"
+    highlights: ["Upgraded Ecommerce App to fully responsive", "SEO Improvements and Management", "Ongoing Theme Maintenance & Support"],
+    link: "https://www.remichel.com/Webservices",
+    beforeAfter: "/images/re_before_after.jpg",
+    modalIDbutton: '#remichel',
+    modalID: 'remichel'
   },
   {
     id: 2,
     name: "Airtemp",
     imgURL: "/images/airtemp.jpg",
     description: "Airtemp indoor comfort systems are designed, assembled and repeatedly and thoroughly tested in North America.",
-    highlights: ["Upgraded to fully responsive", "SEO Improvements and Management", "Ongoing Theme Maintenance & Support"],
-    link: "https://airtemp.com/"
+    highlights: ["Upgraded App to fully responsive", "SEO Improvements and Management", "Ongoing Theme Maintenance & Support"],
+    link: "https://airtemp.com/",
+    beforeAfter: "/images/at_before_after.jpg",
+    modalIDbutton: '#airtemp',
+    modalID: 'airtemp'
   },
   {
     id: 3,
     name: "The Photo Booth Lady",
-    imgURL: "/images/pbl-logo.png",
+    imgURL: "/images/pbl-logo.jpg",
     description: "The Photo Booth Lady is a family owned business, based in the Carolinas, that supply photo booths to parties, weddings and other functions.",
-    highlights: ["Designed and coded completely in PHP and MySql using the Laravel framework", "SEO Setup and Management", "Ongoing Theme Maintenance & Support"],
-    link: "https://boiling-oasis-36668.herokuapp.com/"
+    highlights: ["Full stack app coded completely in PHP and MySql using the Laravel framework from scratch", "SEO Setup and Management", "Ongoing Theme Maintenance & Support"],
+    link: "https://boiling-oasis-36668.herokuapp.com/",
+    beforeAfter: "/images/pbl_before_after.jpg",
+    modalIDbutton: '#pbl',
+    modalID: 'pbl'
+  },
+  {
+    id: 3,
+    name: "Evolve - M. Pennington",
+    imgURL: "/images/evolve-logo.jpg",
+    description: "\"EVOLVE...From Heart Breaks to Hearts Healed\" is a book written by a client.",
+    highlights: ["Full stack Ecommerce app coded completely in PHP and MySql using the Laravel framework from scratch", "SEO Setup and Management", "Ongoing Theme Maintenance & Support"],
+    link: "http://www.michellewpennington.com/",
+    beforeAfter: "/images/evolve_before_after.jpg",
+    modalIDbutton: '#evolve',
+    modalID: 'evolve'
   }
 ]
 
