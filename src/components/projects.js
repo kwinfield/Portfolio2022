a6ba12f9c7663f6742915eb985e4967c1a79af65
KEITH WINFIELD <30985734+kwinfield@users.noreply.github.com>
2025-01-21 16:43:49 -0500

    adjusting card images

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,8 +2,8 @@ const projects = [
   {
     id: 1,
     name: "R.E. Michel",
-    staticImg = '/images/static.png',
-    animatedGif = '/images/test_site_gif.gif',
+    staticImg:'/images/static.png',
+    animatedGif: '/images/test_site_gif.gif',
     imgURL: "/images/remlogo.png",
     description: "Founded in 1935, R.E. Michel is a family business that has more than 290 sales locations coast to coast.",
     highlights: ["Upgraded Ecommerce App to fully responsive", "SEO Improvements and Management", "Ongoing Theme Maintenance & Support"],
@@ -15,6 +15,8 @@ const projects = [
   {
     id: 2,
     name: "Airtemp",
+    staticImg:'/images/airtemp_cover.png',
+    animatedGif: '/images/airtemp_site_gif.gif',
     imgURL: "/images/airtemp.jpg",
     description: "Airtemp indoor comfort systems are designed, assembled and repeatedly and thoroughly tested in North America.",
     highlights: ["Upgraded App to fully responsive", "SEO Improvements and Management", "Ongoing Theme Maintenance & Support"],
@@ -26,6 +28,8 @@ const projects = [
   {
     id: 3,
     name: "The Photo Booth Lady",
+    staticImg:'/images/pb_cover.png',
+    animatedGif: '/images/pg_site_gif.gif',
     imgURL: "/images/pbl-logo.jpg",
     description: "The Photo Booth Lady is a family owned business, based in the Carolinas, that supply photo booths to parties, weddings and other functions.",
     highlights: ["Full stack app coded completely in PHP and MySql using the Laravel framework from scratch", "SEO Setup and Management", "Ongoing Theme Maintenance & Support"],
