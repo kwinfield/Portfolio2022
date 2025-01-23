33c2e88bb2a67b6fe5c82a1b67b264099d026139
KEITH WINFIELD <30985734+kwinfield@users.noreply.github.com>
2025-01-23 15:31:18 -0500

    adjusting card images

--- src/components/middle/portfolio/cards/card2.js
+++ src/components/middle/portfolio/cards/card2.js
@@ -13,45 +13,135 @@ function Card() {
   return (
     <div className='row'>
         {projects.map(project => {
-            return <div className="card col-12 col-xl-3 mb-2">
-                        <img onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="card-img-top img-fluid pt-2" src={isHovering ? project.animatedGif: project.staticImg} alt="Card image cap" style={{ height: '10rem', width: '12.5rem'}}/>
-                        <div className="card-body">
-                            <h5 className="card-title text-center">{project.name}</h5>
-                            <div className="text-center">
-                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target={project.modalIDbutton}>
-                                Project Info
-                                </button>
+            return <div>
+                        <div className="card col-12 col-xl-3 mb-2">
+                            <img onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="card-img-top img-fluid pt-2" src={isHovering ? '/images/test_site_gif.gif': '/images/static.png'} alt="Card image cap" style={{ height: '10rem', width: '12.5rem'}}/>
+                            <div className="card-body">
+                                <h5 className="card-title text-center">{project.name}</h5>
+                                <div className="text-center">
+                                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target={project.modalIDbutton}>
+                                    Project Info
+                                    </button>
+                                </div>
+                            </div>
+                            {/* <!-- Modal --> */}
+                            <div class="modal fade" id={project.modalID} tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLongTitle">{project.name}</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                        <div>
+                                                <div className="row">
+                                                    <div className='col-10 mx-auto mb-5'>
+                                                        <img className="card-img-top img-fluid pt-2" src={project.beforeAfter} alt="Card image cap" />
+                                                    </div>
+                                                </div>
+                                                <p className="card-text text-dark">{project.description}</p>
+                                                <h5>Work Done:</h5>
+                                                <ul className="text-dark">
+                                                    <li>{project.highlights[0]}</li>
+                                                    <li>{project.highlights[1]}</li>
+                                                    <li>{project.highlights[2]}</li>
+                                                </ul> 
+                                                <a href={project.link} className="btn btn-light" target="_blank" rel="noreferrer">Live Project</a>
+                                        </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        {/* <!-- Modal --> */}
-                        <div class="modal fade" id={project.modalID} tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLongTitle">{project.name}</h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                       <div>
-                                            <div className="row">
-                                                <div className='col-10 mx-auto mb-5'>
-                                                    <img className="card-img-top img-fluid pt-2" src={project.beforeAfter} alt="Card image cap" />
+                        <div className="card col-12 col-xl-3 mb-2">
+                            <img onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="card-img-top img-fluid pt-2" src={isHovering ? '/images/airtemp_site_gif.gif': '/images/airtemp_cover.png'} alt="Card image cap" style={{ height: '10rem', width: '12.5rem'}}/>
+                            <div className="card-body">
+                                <h5 className="card-title text-center">{project.name}</h5>
+                                <div className="text-center">
+                                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target={project.modalIDbutton}>
+                                    Project Info
+                                    </button>
+                                </div>
+                            </div>
+                            {/* <!-- Modal --> */}
+                            <div class="modal fade" id={project.modalID} tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLongTitle">{project.name}</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                        <div>
+                                                <div className="row">
+                                                    <div className='col-10 mx-auto mb-5'>
+                                                        <img className="card-img-top img-fluid pt-2" src={project.beforeAfter} alt="Card image cap" />
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <p className="card-text text-dark">{project.description}</p>
-                                            <h5>Work Done:</h5>
-                                            <ul className="text-dark">
-                                                <li>{project.highlights[0]}</li>
-                                                <li>{project.highlights[1]}</li>
-                                                <li>{project.highlights[2]}</li>
-                                            </ul> 
-                                            <a href={project.link} className="btn btn-light" target="_blank" rel="noreferrer">Live Project</a>
-                                       </div>
+                                                <p className="card-text text-dark">{project.description}</p>
+                                                <h5>Work Done:</h5>
+                                                <ul className="text-dark">
+                                                    <li>{project.highlights[0]}</li>
+                                                    <li>{project.highlights[1]}</li>
+                                                    <li>{project.highlights[2]}</li>
+                                                </ul> 
+                                                <a href={project.link} className="btn btn-light" target="_blank" rel="noreferrer">Live Project</a>
+                                        </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        </div>
                                     </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card col-12 col-xl-3 mb-2">
+                            <img onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="card-img-top img-fluid pt-2" src={isHovering ? '/images/pg_site_gif.gif': '/images/pb_cover.png'} alt="Card image cap" style={{ height: '10rem', width: '12.5rem'}}/>
+                            <div className="card-body">
+                                <h5 className="card-title text-center">{project.name}</h5>
+                                <div className="text-center">
+                                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target={project.modalIDbutton}>
+                                    Project Info
+                                    </button>
+                                </div>
+                            </div>
+                            {/* <!-- Modal --> */}
+                            <div class="modal fade" id={project.modalID} tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLongTitle">{project.name}</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div>
+                                                <div className="row">
+                                                    <div className='col-10 mx-auto mb-5'>
+                                                        <img className="card-img-top img-fluid pt-2" src={project.beforeAfter} alt="Card image cap" />
+                                                    </div>
+                                                </div>
+                                                <p className="card-text text-dark">{project.description}</p>
+                                                <h5>Work Done:</h5>
+                                                <ul className="text-dark">
+                                                    <li>{project.highlights[0]}</li>
+                                                    <li>{project.highlights[1]}</li>
+                                                    <li>{project.highlights[2]}</li>
+                                                </ul> 
+                                                <a href={project.link} className="btn btn-light" target="_blank" rel="noreferrer">Live Project</a>
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
