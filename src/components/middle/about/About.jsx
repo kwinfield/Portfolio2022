915f9b99b13b5da541b83d3c6309214c7e63a1c2
KEITH WINFIELD <30985734+kwinfield@users.noreply.github.com>
2025-01-24 11:17:52 -0500

    change about me section to top to bottom

--- src/components/middle/about/About.jsx
+++ src/components/middle/about/About.jsx
@@ -5,11 +5,11 @@ const About = () => (
     <div className="about" id="about">
         <h1>About Me</h1>
         <h5>My Story</h5>
-        <div className="row pt-5">
-            <div className="col-md-6">
+        <div className="pt-5">
+            <div>
                 <p>I’m a Web Developer based in Baltimore, Maryland, specializing in user-centric design and modern front-end technologies. My role as a Web Manager for a nationwide wholesale supplier has given me extensive experience running digital marketing campaigns—leveraging tools like Google Analytics, Google Ads, and Mailchimp—to drive engagement and conversions.</p>
             </div>
-            <div className="col-md-6">
+            <div>
                 <p>My background in digital, direct, and customer relationship marketing dates back to the early 2000s, when I was a young creator helping small start-ups establish their web presence. I’ve always enjoyed taking things apart and rebuilding them—an inquisitiveness that continues to drive my passion for learning emerging technologies. ​</p>
             </div>
         </div>
