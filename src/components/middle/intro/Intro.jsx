d05fd04f03a2d79c535a6e6ed6de7ed0aaac49f5
Keith Winfield <keithwinfield@Keiths-MacBook-Pro.local>
2026-04-23 14:06:24 -0400

    added build info

--- src/components/middle/intro/Intro.jsx
+++ src/components/middle/intro/Intro.jsx
@@ -10,6 +10,7 @@ const Intro = () => (
             <div className="message">
                 <h1 className="animate__animated animate__bounceInDown animate__delay-1s">Web Developer</h1>
                 <p className="animate__animated animate__bounceInRight animate__delay-2s">WordPress, PHP, and Content Management specialist focused on website updates, SEO implementation, and maintaining production websites.</p>
+                <p className="animate__animated animate__bounceInLeft animate__delay-2s">This portfolio is built using React and dynamically renders content through API integration.</p>
             </div>
             <a href="#contact" className="btn btn-lg btn-outline-light mt-5 animate__animated animate__bounceInUp animate__delay-3s big-btn">Contact Me </a>
             <a href="#contact" className="btn btn-lg btn-outline-light mt-5 sm-btn">Contact Me </a>
